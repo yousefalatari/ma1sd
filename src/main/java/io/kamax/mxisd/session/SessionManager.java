f042b82a5075a7e3f939b65f6e8519d6272bc3d5
eyecreate <eyecreate@gmail.com>
2019-10-21 16:06:23 -0400

    add missing import

--- src/main/java/io/kamax/mxisd/session/SessionManager.java
+++ src/main/java/io/kamax/mxisd/session/SessionManager.java
@@ -57,6 +57,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Calendar;
