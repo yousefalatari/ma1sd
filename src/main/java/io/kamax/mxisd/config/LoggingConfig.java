75efd9921d6179c1c9a20524902ec715ead1d2ca
Anatoly Sablin <sablintolya@gmail.com>
2020-01-28 00:55:39 +0300

    Improve logging configuration. Introduce the root and the app log levels.

--- src/main/java/io/kamax/mxisd/config/LoggingConfig.java
+++ src/main/java/io/kamax/mxisd/config/LoggingConfig.java
@@ -6,9 +6,10 @@ import org.slf4j.LoggerFactory;
 
 public class LoggingConfig {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger("App");
 
     private String root;
+    private String app;
 
     public String getRoot() {
         return root;
@@ -18,14 +19,27 @@ public class LoggingConfig {
         this.root = root;
     }
 
+    public String getApp() {
+        return app;
+    }
+
+    public void setApp(String app) {
+        this.app = app;
+    }
+
     public void build() {
-        String systemLevel = System.getProperty("org.slf4j.simpleLogger.log.io.kamax.mxisd");
         LOGGER.info("Logging config:");
-        if (StringUtils.isNotBlank(systemLevel)) {
-            LOGGER.info("  Logging level set by environment: {}", systemLevel);
-        } else if (StringUtils.isNotBlank(getRoot())) {
-            LOGGER.info("  Logging level set by the configuration: {}", getRoot());
-            System.setProperty("org.slf4j.simpleLogger.log.io.kamax.mxisd", getRoot());
+        if (StringUtils.isNotBlank(getRoot())) {
+            LOGGER.info("  Default log level: {}", getRoot());
+            System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", getRoot());
+        }
+
+        String appLevel = System.getProperty("org.slf4j.simpleLogger.log.io.kamax.mxisd");
+        if (StringUtils.isNotBlank(appLevel)) {
+            LOGGER.info("  Logging level set by environment: {}", appLevel);
+        } else if (StringUtils.isNotBlank(getApp())) {
+            System.setProperty("org.slf4j.simpleLogger.log.io.kamax.mxisd", getApp());
+            LOGGER.info("  Logging level set by the configuration: {}", getApp());
         } else {
             LOGGER.info("  Logging level hasn't set, use default");
         }
