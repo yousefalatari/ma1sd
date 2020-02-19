072e5f66cbd9e73d92a8586740e97056cb3248b0
Anatoliy Sablin <tolya@sablin.xyz>
2020-02-19 23:35:59 +0300

    #26 Use empty pepper.

--- src/main/java/io/kamax/mxisd/hash/engine/NoneEngine.java
+++ src/main/java/io/kamax/mxisd/hash/engine/NoneEngine.java
@@ -1,13 +1,11 @@
 package io.kamax.mxisd.hash.engine;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoneEngine implements Engine {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NoneEngine.class);
-    private static final String PEPPER = RandomStringUtils.random(8, true, true);
 
     @Override
     public void updateHashes() {
@@ -16,6 +14,6 @@ public class NoneEngine implements Engine {
 
     @Override
     public String getPepper() {
-        return PEPPER;
+        return "";
     }
 }
